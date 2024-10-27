a42a4c52b98bd2cbdd663ee7f1efc239cca87807
Waland2 <cool.drakon@gmail.com>
2024-10-27 15:57:43 +0300

    add static

--- aniton/aniton/urls.py
+++ aniton/aniton/urls.py
@@ -1,6 +1,8 @@
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -9,4 +11,4 @@ urlpatterns = [
     path('api/v1/task/', include('task.urls')),
     path('api/v1/upgrade/', include('upgrade.urls')),
     path('api/v1/friend/', include('friend.urls')),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
